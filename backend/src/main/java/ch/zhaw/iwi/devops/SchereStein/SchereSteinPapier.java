73e6ee17b542b9a92ac336af8593595c9a8eb368
Mato Bajusic <bajusmat@students.zhaw.ch>
2022-04-08 14:14:11 +0200

    Test1: Schere vs Stein

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/SchereStein/SchereSteinPapier.java
@@ -0,0 +1,11 @@
+package ch.zhaw.iwi.devops.SchereStein;
+
+public class SchereSteinPapier {
+    public String Convert(String SpielerA, String SpielerB){
+        if(SpielerA == "Schere" && SpielerB == "Stein"){
+            return "Stein gewinnt";
+        }
+        return "";
+    }
+    
+}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/SchereSteinPapierTest.java
@@ -0,0 +1,15 @@
+package ch.zhaw.iwi.devops;
+import org.junit.Assert;
+import org.junit.Test;
+
+import ch.zhaw.iwi.devops.SchereStein.SchereSteinPapier;
+
+public class SchereSteinPapierTest {
+    @Test
+    public void SchereVerliertStein(){
+        SchereSteinPapier eingabe = new SchereSteinPapier();
+        String result = eingabe.Convert("Schere", "Stein");
+        Assert.assertEquals("Stein gewinnt", result);
+    }
+    
+}
